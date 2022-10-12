54b4fa57e8ccf72faeedaf4ea17760e46f0d0ff2
Sandra Truong <105508619+sandrat1790@users.noreply.github.com>
2022-10-12 13:52:29 -0600

    Add files via upload

---
+++ enemiesAnimation/enemyTwoScript.js
@@ -0,0 +1,65 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 10;
+const enemiesArr = [];
+
+let gameFrame = 0;
+
+class Enemy {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "enemy2.png";
+
+    //this.speed = Math.random() * 4 - 2;
+    this.characterWidth = 293;
+    this.characterHeight = 155;
+    this.width = this.characterWidth / 2.5;
+    this.height = this.characterHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+  }
+  update() {
+    this.x += Math.random() * 15 - 7.5;
+    this.y += Math.random() * 10 - 5;
+
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.characterWidth,
+      0,
+      this.characterWidth,
+      this.characterHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+for (let index = 0; index < numberOfEnemies; index++) {
+  enemiesArr.push(new Enemy());
+}
+console.log(enemiesArr);
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  enemiesArr.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
